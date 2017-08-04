81caab774958027b4abeb7795dfbd55f99b16718
qyf <1753930534@qq.com>
2017-08-04 21:02:27 +0800

    添加完成和删除功能

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import './reset.css';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import 'normalize.css';
+  
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+      ]
+    }
+  }
   render() {
+
+    let todos = this.state.todoList
+       .filter((item)=> !item.deleted)
+       .map((item,index)=>{
+      return ( 
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
+            onDelete={this.delete.bind(this)}/>
+        </li>
+      )
+    })
+    console.log(todos)
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+            <TodoInput content={this.state.newTodo} 
+             onChange={this.changeTitle.bind(this)}
+             onSubmit={this.addTodo.bind(this)} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ol className="todoList">
+          {todos}
+        </ol>
       </div>
-    );
+    )
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state) 
+  }
+
+  changeTitle(event){
+      this.setState({
+        newTodo: event.target.value,
+        todoList: this.state.todoList
+      })
+  }
+
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state) 
   }
 }
 
 export default App;
+
+let id = 0
+ 
+function idMaker(){
+  id += 1
+  return id
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App'
+
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
+  <App />,
   document.getElementById('root2')
-);
+)
